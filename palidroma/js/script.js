6e288bbee53aa04e202108091f37bd921685e390
jacopo35 <jaczor00@gmail.com>
2021-11-29 15:14:15 +0100

    add-function-palidromaJs

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -1,5 +1,17 @@
 /* Palidroma
    Chiedere all’utente di inserire una parola
    Creare una funzione per capire se la parola inserita è palindroma
-   (prima senza funzione e poi con funzione)
+   (prima senza funzione e poi con funzione).
 */
+
+function reverseStr(string) {
+ let reverseString = "";
+ for (let i = string.length - 1; i >= 0; i--) {
+      reverseString += string[i];
+   }
+ return reverseString;
+}
+
+let string = prompt('Inserisci una parola');
+
+
