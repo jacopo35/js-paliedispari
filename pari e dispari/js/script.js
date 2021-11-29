4ea7a774ccce45a3991f6626fe2ae74aee610f65
jacopo35 <jaczor00@gmail.com>
2021-11-29 16:08:55 +0100

    add-complete-js-Dadi

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -23,3 +23,35 @@ function isEven(num) {
         return false;
     }
 }
+
+let user = prompt('Scrivi pari o dispari');
+
+while (user != "pari" && user != "dispari") {
+    user = prompt('Scrivi pari o dispari');
+}
+
+let numberUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
+
+while (numberUser < 0 || numberUser > 5) {
+    numberUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
+}
+
+//Creare un numero a caso e sommarlo con il numero dell'utente:
+
+let numberPc = randNum(1, 5);
+let sum = numberUser + numberPc;
+
+//Mostrare le scelte dell'utente, il numero a caso e la somma:
+
+ console.log('Utente: ' + user);
+ console.log('Nome scelto da utente: ' + numberUser);
+ console.log('Nome random del PC: ' + numberPc);
+ console.log('Somma: ' + sum);
+
+//Controllare chi ha vinto:
+
+if (isEven(sum) == true && user == "pari" || isEven(sum) == false && user == "dispari") {
+    console.log('Ha vinto utente');
+} else {
+    console.log('Ha vinto il PC');
+}
